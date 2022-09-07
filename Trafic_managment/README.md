4777444944ca1146cbc480bbdb5eeb6dbd964428
Pratham Patel <prpatel43@myseneca.ca>
2022-09-07 16:57:33 -0400

    update

---
+++ Trafic_managment/README.md
@@ -0,0 +1,31 @@
+### Part A
+1.	In a browser, load a webpage from your Apache server
+2.	Transfer a file via FTP to your client
+3.	RDP into your Windows server
+4.	SSH into your router
+5.	Login to your MySQL database server
+
+#### Ip table file link :- 
+
+### Part A2
+1.	APACHE by destination port
+2.	MySQL by source port
+3.	SSH to LS-XX by destination port
+
+#### Ip table file link :- 
+
+### Part B
+1.	In a browser, load a webpage from your IIS server
+2.	RDP into your Windows server
+3.	SSH into your Linux server
+4.	Login to your MySQL database server
+
+#### Ip table file link :- 
+
+### Part B2
+1.	SSH to your Linux server by source port
+2.	RDP to your Windows server by source port
+
+#### Ip tabel file link : -
+
+
---
+++ Trafic_managment/file_and_video/Final-Eval-1.sh
@@ -0,0 +1,52 @@
+#Restore Default Rules
+iptables -F
+iptables -t nat -F
+iptables -X
+#iptables -P INPUT DROP
+iptables -P FORWARD DROP
+#iptables -P OUTPUT DROP
+
+#INPUT Starting Rules
+iptables -A INPUT -p tcp -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A INPUT -p tcp -s 10.22.70.0/24 -m state --state NEW --dport 2226 -j ACCEPT
+
+#DNS
+iptables -A FORWARD -p tcp -d 172.17.31.36 --dport 53 -j ACCEPT
+iptables -A FORWARD -p tcp -s 172.17.31.36 --sport 53 -j ACCEPT
+iptables -A FORWARD -p udp -d 172.17.31.36 --dport 53 -j ACCEPT
+iptables -A FORWARD -p udp -s 172.17.31.36 --sport 53 -j ACCEPT
+
+#RDP
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.36 --dport 3389 -j ACCEPT
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.36 -d 10.22.70.0/24 --sport 3389 -j ACCEPT
+
+#MySQL
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.37 --dport 3306 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 3306 -m limit --limit 10/min -j LOG --log-prefix "DROPPED-MYSQL"
+#iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.37 -d 10.22.70.0/24 --sport 3306 -j ACCEPT
+
+#Apache
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.37 --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 21 -m limit --limit 10/min -j LOG --log-prefix "DROPPED-APACHE"
+#iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.37 -d 10.22.70.0/24 --sport 80 -j ACCEPT
+
+#IIS
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.36 --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.36 -d 10.22.70.0/24 --sport 80 -j ACCEPT
+
+#FTP (Administration Port)
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.36 --dport 21 -j ACCEPT
+
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.36 -d 10.22.70.0/24 --sport 21 -j ACCEPT
+
+#FTP (Data ports)
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.36 --dport 40000:41000 -j ACCEPT
+iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.36 -d 10.22.70.0/24 --sport 40000:41000 -j ACCEPT
+
+#SSH
+iptables -A FORWARD -p tcp -m state --state NEW,ESTABLISHED -s 10.22.70.0/24 -d 172.17.31.37 --dport 22 -j ACCEPT
+iptables -A FORWARD -p tcp --sport 22 -m limit --limit 10/min -j LOG --log-prefix "DROPPED-SSH"
+#iptables -A FORWARD -p tcp -m state --state ESTABLISHED,RELATED -s 172.17.31.37 -d 10.22.70.0/24 --sport 22 -j ACCEPT
+
